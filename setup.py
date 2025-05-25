6abc6b2edd0158be5f310c45884302eee936864e
dzeckelev <dzeckelev@gmail.com>
2025-05-25 22:49:47 +0300

    add pkgs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="voicefilter",
     version="0.1.0",
     description="Speaker-conditioned VoiceFilter-Lite implementation",
-    packages=find_packages(),         # найдёт модуль voicefilter и подмодули
+    packages=find_packages(),
     install_requires=[
         "torch>=1.7",
         "torchaudio",
