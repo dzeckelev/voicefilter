5f06bc9317671b1375481b84b5f231918f6fcc03
dzeckelev <dzeckelev@gmail.com>
2025-05-25 22:36:07 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="voicefilter-lite",
+    name="voicefilter",
     version="0.1.0",
-    packages=find_packages(),
+    description="Speaker-conditioned VoiceFilter-Lite implementation",
+    packages=find_packages(),         # найдёт модуль voicefilter и подмодули
     install_requires=[
-        "torch>=1.12",
+        "torch>=1.7",
         "torchaudio",
     ],
     include_package_data=True,
